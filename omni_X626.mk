a7f68147996770a04fd5ee03a815ef96903a46b5
dmurimi14 <d.murimi14@gmail.com>
2019-09-18 19:04:45 +0300

    Rebased the TWRP Tree
    Signed-off-by: dmurimi14 <d.murimi14@gmail.com>

--- omni_X626.mk
+++ omni_X626.mk
@@ -11,11 +11,15 @@ $(call inherit-product, vendor/omni/config/common.mk)
 PRODUCT_BRAND := Infinix
 PRODUCT_DEVICE := X626
 PRODUCT_MANUFACTURER := INFINIX MOBILITY LIMITED
-PRODUCT_MODEL := Infinix-X626
+PRODUCT_MODEL := Infinix X626
 PRODUCT_NAME := omni_X626
 
 # enable stock zip packages flash
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.treble.enabled=true \
+    persist.sys.usb.config=mtp \
+    persist.service.adb.enable=1 \
+    persist.service.debuggable=1 \
     ro.secure=1 \
     ro.adb.secure=0 \
     ro.allow.mock.location=0
